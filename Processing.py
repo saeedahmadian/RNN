80ec1e704ae3d320dbaa3754fffe26fc7f9ce413
saeed Ahmadian <38537212+saeedahmadian@users.noreply.github.com>
2019-09-04 19:29:54 -0500

    Update Processing.py

--- Processing.py
+++ Processing.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import csv
 from datetime import datetime
+import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose as decomp
+import statsmodels.api as sm
 
 # df = pd.read_csv('Saeed.csv',header=3, delimiter=";")
 
@@ -23,8 +27,8 @@ with open('Saeed.csv') as csvfile:
 
 
 columns += columns[3:]
-gas_col = list(map(lambda i : 'GAS_'+i,columns[3:9]))
-temp_col = list(map(lambda i : 'Temp_'+i,columns[9:]))
+gas_col = list(map(lambda i : 'GAS_Pressure'+i,columns[3:9]))
+temp_col = list(map(lambda i : 'GAS_Temperature_'+i,columns[9:]))
 col = gas_col+temp_col
 # col.insert(0,columns[0])
 
@@ -44,11 +48,88 @@ dateind = pd.date_range(start='21/4/2018', end='21/06/2018')
 df.index =dateind
 df.replace(0,np.nan).to_csv('raw_data.csv')
 df = df.replace(0,np.nan).interpolate('linear',0)
-
+df_new_MA_press = df - df.rolling(7).mean()
+df_new_EWM_press = df - df.ewm(halflife=7,ignore_na=False,min_periods=0).mean()
+df_first = df.diff(1)
+df_second = df.diff(2)
+df_log = np.log(df)
+df_log_MA = df_log- df_log.rolling(7).mean()
 # for ind,element in enumerate(columns,start=0):
 #     if ind < 2 and ind <9:
 #         new_col.append('GAS_'+element)
 #     elif ind > 8:
 #         new_col.append('Temp_' + element)
+
+def Stationarytest(TS):
+    test = adfuller(TS, autolag='AIC')
+    result = pd.Series(test[0:4], index=['Test Statistic', 'p-value', '#Lags Used', 'Number of Observations Used'])
+    for key, value in test[4].items():
+        result['Critical Value (%s)' % key] = value
+    return result
+def Decomposition(TS, nameOfTS, model_name='additive',frequency=None):
+    decomposed= decomp(x=TS,model=model_name, freq= frequency)
+    out= pd.DataFrame()
+    out['Original_'+nameOfTS] = TS
+    out["trend"] = decomposed.trend
+    out["Seasonality"] = decomposed.seasonal
+    out["residual"]= decomposed.resid
+    return out
+
+def plot_decomposition(Decomp_TS):
+    """
+    """
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(15, 8),sharex=True)
+
+    ax1.plot(Decomp_TS.iloc[:,0], label=Decomp_TS.columns[0], color='black')
+    ax1.legend(loc='best')
+    ax1.tick_params(axis='x', rotation=45)
+
+    ax2.plot(Decomp_TS.iloc[:,1], label='Trend Element', color='magenta')
+    ax2.legend(loc='best')
+    ax2.tick_params(axis='x', rotation=45)
+
+    ax3.plot(Decomp_TS.iloc[:,2], label='Seasonality Element', color='green')
+    ax3.legend(loc='best')
+    ax3.tick_params(axis='x', rotation=45)
+
+    ax4.plot(Decomp_TS.iloc[:,3], label='Residuals Element', color='red')
+    ax4.legend(loc='best')
+    ax4.tick_params(axis='x', rotation=45)
+    plt.tight_layout()
+    TSname=Decomp_TS.columns[0]
+    # Show graph
+    # plt.suptitle('Trend, Seasonal, and Residual Decomposition of %s' % (TSname),
+    #              x=0.5,
+    #              y=1.05,
+    #              fontsize=18)
+    # plt.show()
+    plt.savefig(TSname + '.png')
+    plt.close()
+
+def TimeSerieOLS(TS,lag):
+    TS_lag = TS.shift(lag)
+    TS_lag = TS_lag.dropna()
+    TS = TS.iloc[lag:,:]
+    res = []
+    newdata=[]
+    for col in TS.columns:
+        res.append(sm.OLS(TS[col], TS_lag[col]).fit())
+        tmp = pd.DataFrame(data=sm.OLS(TS[col], TS_lag[col]).fit().predict(), index=TS[col].index,columns=[col+'_prediction'])
+        tmp[col]=TS[col]
+        tmp[col +'_prediction_error'] = tmp[col]-tmp[col + '_prediction']
+        newdata.append(tmp)
+    return res,newdata
+
+
+def ErrorOLS(err,lag):
+    err=pd.DataFrame(data=err,columns=[err.name])
+    err_lag = err.shift(lag).dropna()
+    err = err.iloc[lag:,:]
+    model = []
+    res = []
+    for col in err.columns:
+        model.append(sm.OLS(err[col],err_lag[col]))
+        res.append(sm.OLS(err[col],err_lag[col]).fit())
+    return res
 
 a=1
